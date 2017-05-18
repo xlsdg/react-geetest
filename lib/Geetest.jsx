32243b9e9b6d3e486e92437d67b764135c5b6605
xLsDg <xlsdg@qq.com>
2017-05-18 11:06:16 +0800

    chore(mod): upgrade modules

--- lib/Geetest.jsx
+++ lib/Geetest.jsx
@@ -6,65 +6,142 @@ class Geetest extends React.Component {
   constructor(props) {
     // console.log('constructor', props);
     super(props);
-    this.state = {};
+    this.state = {
+      ins: null,
+      script: null
+    };
     this._init = this._init.bind(this);
+    this._ready = this._ready.bind(this);
+    this._load = this._load.bind(this);
+    this._destroy = this._destroy.bind(this);
   }
   // componentWillMount() {
-    // console.log('componentWillMount');
+    // const that = this;
+    // console.log('componentWillMount', that.props, that.state);
   // }
   componentDidMount() {
-    // console.log('componentDidMount');
-    if (this.props.challenge) {
-      this._init();
-    }
+    const that = this;
+    // console.log('componentDidMount', that.props, that.state);
+    that._init();
   }
   // componentWillReceiveProps(nextProps) {
-    // console.log('componentWillReceiveProps', nextProps);
+    // const that = this;
+    // console.log('componentWillReceiveProps', that.props, nextProps);
   // }
   shouldComponentUpdate(nextProps, nextState) {
-    // console.log('shouldComponentUpdate', nextProps, nextState);
-    return nextProps.challenge !== this.props.challenge;
+    const that = this;
+    // console.log('shouldComponentUpdate', that.props, nextProps, that.state, nextState);
+    return nextProps.challenge !== that.props.challenge;
   }
   // componentWillUpdate(nextProps, nextState) {
-    // console.log('componentWillUpdate', nextProps, nextState);
+    // const that = this;
+    // console.log('componentWillUpdate', that.props, nextProps, that.state, nextState);
   // }
   componentDidUpdate(prevProps, prevState) {
-    // console.log('componentDidUpdate', prevProps, prevState);
-    if (this.props.challenge) {
-      this._init();
-    }
+    const that = this;
+    // console.log('componentDidUpdate', prevProps, that.props, prevState, that.state);
+    that._init();
   }
-  // componentWillUnmount() {
-    // console.log('componentWillUnmount');
-  // }
+  componentWillUnmount() {
+    const that = this;
+    // console.log('componentWillUnmount', that.props, that.state);
+    that._destroy();
+  }
   _init() {
+    const that = this;
     // console.log('_init');
-    let that = this;
+
+    if (window.initGeetest) {
+      return that._ready();
+    }
+
+    const ds = document.createElement('script');
+    ds.type = 'text/javascript';
+    ds.async = true;
+    ds.charset = 'utf-8';
+    if (ds.readyState) {
+      ds.onreadystatechange = function() {
+        if (ds.readyState === 'loaded' || ds.readyState === 'complete') {
+          ds.onreadystatechange = null;
+          that._ready();
+        }
+      };
+    } else {
+      ds.onload = function() {
+        ds.onload = null;
+        that._ready();
+      };
+    }
+    ds.src = `${document.location.protocol}//static.geetest.com/static/tools/gt.js?_t=${(new Date()).getTime()}`;
+    const s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ds, s);
+
+    that.setState({
+      script: ds
+    });
+  }
+  _ready() {
+    const that = this;
+    // console.log('_ready');
+    const {
+      gt, challenge, https, product, lang, sandbox, width, success
+    } = that.props;
+    const {
+      ins
+    } = that.state;
+
+    if (!window.initGeetest) {
+      return;
+    }
+
+    if (ins) {
+      return that._load(ins);
+    }
+
     return window.initGeetest({
-      gt: that.props.gt,
-      challenge: that.props.challenge,
-      https: that.props.https,
-      product: that.props.product,
-      lang: that.props.lang,
-      sandbox: that.props.sandbox,
-      width: that.props.width,
-      offline: !that.props.success
+      gt, challenge, https, product, lang, sandbox, width, offline: !success
     }, function(geetest) {
-      geetest.appendTo(ReactDOM.findDOMNode(that));
-      geetest.onReady(that.props.onReady);
-      geetest.onRefresh(that.props.onRefresh);
-      geetest.onSuccess(function() {
-        let value = geetest.getValidate();
-        that.props.onSuccess(value);
+      that._load(geetest);
+
+      that.setState({
+        ins: geetest
       });
-      geetest.onFail(that.props.onFail);
-      geetest.onError(that.props.onError);
+    });
+  }
+  _load(ins) {
+    const that = this;
+    // console.log('_load');
+    const {
+      onReady, onRefresh, onSuccess, onFail, onError
+    } = that.props;
+
+    ins.appendTo(ReactDOM.findDOMNode(that));
+    ins.onReady(onReady);
+    ins.onRefresh(onRefresh);
+    ins.onSuccess(() => onSuccess(ins.getValidate()));
+    ins.onFail(onFail);
+    ins.onError(onError);
+  }
+  _destroy() {
+    const that = this;
+    // that.state.script.parentNode.removeChild(that.state.script);
+    that.setState({
+      ins: null,
+      script: null
     });
   }
   render() {
+    const that = this;
     // console.log('render');
+    const {
+      challenge
+    } = that.props;
+
     return (
-      <div key="geetest-captcha" />
+      <div
+        className="i-geetest"
+        key={challenge}
+      />
     );
   }
 }
